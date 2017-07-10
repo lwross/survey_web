9a47baf53830d472e43bd895197e95979083ebec
Lachlan Ross <lachlan.ross@salesforce.com>
2017-07-10 19:23:09 +1000

    no message

--- index.php
+++ index.php
@@ -8,178 +8,103 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" data-requiremodule="jQuery" type="text/javascript"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" data-requiremodule="jQuery" type="text/javascript"></script>
     <link href="./css/bootstrap.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-
-      @media (max-width: 980px) {
-        /* Enable use of floated navbar text */
-        .navbar-text.pull-right {
-          float: none;
-          padding-left: 5px;
-          padding-right: 5px;
-        }
-      }
-    </style>
     <link href="./css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src=".js/html5shiv.js"></script>
-    <![endif]-->
-
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
 google.charts.load('current', {'packages':['gauge','corechart']});
 
-//Start: ******DONUT Chart***************// 
- function drawDonutChart(Value1, Value2, Value3) {
-
-         // Create the data table for Anthony's pizza.
-  var data = google.visualization.arrayToDataTable([
-            ['Category', 'Score'],
-            ['Crystal Clear',Value1],
-            ['Dull Appearance',Value2],
-            ['Visible Rubbish',Value3],
-          ]);
-
-          var options = {
-            pieHole: 0.3,
-            pieSliceTextStyle: {
-            color: 'black',
-            },
-            legend: 'none',
-            backgroundColor: { fill:'transparent' },
-            slices: {
-              0: { color: '#66FD77' },
-              1: { color: '#FDF565' },
-              2: { color: '#FD6764' }
-            },
-            /*title: 'Survey Results' , */
-            width: 350,
-            height: 350,
-            chartArea: { width: '90%', height: '90%'},
-   };
-
-        var chart = new google.visualization.PieChart(document.getElementById('DonutChart'));
-        chart.draw(data, options);
-      }
-//End: ******DONUT Chart***************//
-         
 </script>
+
+<script type="text/javascript">
+
+    function drawBarChart(rawdata) {
+
+      var graphdata = [
+        ["Element", "Votes", { role: "style" } ]
+      ];
+
+
+      $.each(teams, function(i, teamrow) {
+        var voteCount = 0;
+        $.each(rawdata, function(j, rawrow) {
+          if (rawrow.Team == i) {
+            voteCount = rawrow.VoteCount;
+            return false;
+          }
+        });
+
+        graphdata.push( [teams[i], parseInt(voteCount), "gold"]);
+      });
+
+      var data = google.visualization.arrayToDataTable(graphdata);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        //title: "Hack to the Future Votes",
+        //width: 1500,
+        height: 800,
+        bar: {groupWidth: "90%"},
+        bars: 'horizontal',
+        hAxis: { ticks: [] },
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("Chart"));
+      chart.draw(view, options);
+  }
+  </script>
 
 <script>
 var lastChecked = "all";
 
-var graphURL = "http://pub.s4.exacttarget.com/tss40r4joam";
-var detailURL = "http://pub.s4.exacttarget.com/wnar1q2ocnu";
+// URL of graph JSON landing page
+var graphURL = "http://pub.s4.exacttarget.com/1fpgn10x51m";
 var c = 0;
 var t;
 var n;
-var Supporter = 0;
-var Passive = 0;
-var Detractor = 0;
+
+var teams = {1: 'Forty Two',
+             2: 'Kakou',
+             3: 'Powered By Coffee',
+             4: 'Lillie Fro',
+             5: 'Foundationeers',
+             6: 'Find Give Keep',
+             7: '50 Reefs',
+             8: 'Cloud Walkers',
+             9: 'Eleu'};
+
 
 function createNotice(t) {
 
-	$.ajax({
-		 crossDomain:true,
-		 url:detailURL+"?time="+t,
-	     dataType: 'jsonp', 
-	     success:function(data){
-	         	$.each(data.result, function(i, result) {
-					$('<div class="alert alert-success">'+result.FullResponse+'</div>').hide().prependTo('#messageArea').slideDown("slow");
-				})
-				lastChecked = data.lastChecked;
-		}
-	});
+  $.ajax({
+     crossDomain:true,
+     url:graphURL+"?time="+t,
+       dataType: 'jsonp',
+       success:function(data){
+          lastChecked = data.lastChecked;
+          google.charts.setOnLoadCallback(drawBarChart(data.result));
+       }
+  });
 
-	$.ajax({
-		 crossDomain:true,
-		 url:graphURL+"?time="+t,
-	     dataType: 'jsonp', 
-	     success:function(data){
-	     Supporter = Passive = Detractor = 0;
-	         	$.each(data.result, function(i, result) {
-            if (result.Response == "1") {
-              Detractor++;
-            } else if (result.Response == "2") {
-              Passive++;
-            } else {
-              Supporter++;
-            }
-				})
-			lastChecked = data.lastChecked;
-
-
-        /* draw chart */
-        if ((Detractor != 0) || (Passive != 0) || (Supporter != 0)) {
-          google.charts.setOnLoadCallback(drawDonutChart(Supporter,Passive,Detractor));
-        }
-		}
-	});
-	
-	n = setTimeout("createNotice(lastChecked)", 5000);
+  n = setTimeout("createNotice(lastChecked)", 5000);
 }
 
 $(function() {
-	n = setTimeout("createNotice(lastChecked)", 5000);
-	$("#messageArea").attr({
-		scrollTop: $("#messageArea").attr("scrollHeight")
-	});
+  n = setTimeout("createNotice(lastChecked)", 5000);
 })
-	
+
 </script>
+</head>
 
-   </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container-fluid">
-          <a class="brand" href="#">Mobile Survey - Please rate todays water quality at Frankston Beach</a>
-        </div>
-      </div>
-    </div>
-    
-    <div class="container-fluid">
-      <div class="row-fluid">
-        <div id="text" class="span4">
-    			<div class="well">
-    				<h4>text</h4><h2>WATER</h2> <h4>to</h4> <h2>0448 002 002</h2>
-    			</div>
-        </div>
-		    <div id="messageAreaChart" class="span8">
-            <center>
-            <div id="DonutChart">
-            </div>
-            </center>
-        </div>
-      </div>
-      
-    <div class="container-fluid">
-      <div class="row-fluid">
-        <div id="text" class="span4">
-        </div>
-		    <div id="messageArea" class="span8">
-        </div>
-      </div>
-
-      <hr>
-
-      <footer>
-        <p>&copy; Salesforce Marketing Cloud 2017</p>
-      </footer>
-    </div>
-  </body>
+<body>
+  <div id="Chart"></div>
+</body>
 </html>
-
-
