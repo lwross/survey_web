783395041ad2507c0a71124480b0900517fc7cc1
Lachlan Ross <lachlan.ross@salesforce.com>
2017-08-17 18:10:51 +1000

    no message

---
+++ lookup.php
@@ -0,0 +1,10 @@
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, 'url_here');
+$result = curl_exec($ch);
+curl_close($ch);
+
+$obj = json_decode($result);
+echo $obj->access_token;
