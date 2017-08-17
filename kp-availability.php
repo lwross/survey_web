9fa5fc9f204e09031790fc67ca458e2b93ee2d35
Lachlan Ross <lachlan.ross@salesforce.com>
2017-08-17 18:40:16 +1000

    no message

--- kp-availability.php
+++ kp-availability.php
@@ -1,6 +1,6 @@
 <?php
-//$combination = strtoupper($_REQUEST['combination']);
-$combination = 'LWR';
+$combination = strtoupper($_REQUEST['combination']);
+//$combination = 'LWR';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
