9ad41cfe9c8d3a768d7715868f00ce6d9eebe9ae
Lachlan Ross <lachlan.ross@salesforce.com>
2018-11-08 19:20:53 +1100

    no message

--- honda-dealership.php
+++ honda-dealership.php
@@ -7,37 +7,37 @@ curl_setopt($ch, CURLOPT_URL, "http://v0.postcodeapi.com.au/suburbs/$postcode.js
 $result = curl_exec($ch);
 curl_close($ch);
 
-//rint "http://v0.postcodeapi.com.au/suburbs/$postcode.json";
 
 $obj = json_decode($result);
-var_dump($obj[0]->latitude);//->Data->Available);
+var_dump($obj[0]);
+
+$lat = $obj[0]->latitude;
+$long = $obj[0]->longitude;
+
+
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch2, CURLOPT_URL, "https://www.honda.com.au/content/honda/en/finddealerapi/jcr:content/find.dealerV2.json/$lat,$long");
+
+$result2 = curl_exec($ch2);
+curl_close($ch2);
+$obj2 = json_decode($result2);
+var_dump($result2);
 exit;
+print "Oops! combination $postcode is unavailable.  Here are some alternatives: ";
 
-if ($obj->Data->Available) {
-	print "Congratulations $postcode is available!  Would you like to purchase this?";
-} else {
+$suggestions = "";
+foreach ($obj2->Data as $arr) {
 
-	$ch2 = curl_init();
-	curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch2, CURLOPT_URL, "https://api.kiwiplates.nz/api//suggest?&startIndex=0&endIndex=8&leetSpeakAlt=true&popularNos=false&rangeOfNumbers=true&permutations=true&viewAll=false&combination=$combination");
-	$result2 = curl_exec($ch2);
-	curl_close($ch2);
-	$obj2 = json_decode($result2);
-	print "Oops! combination $postcode is unavailable.  Here are some alternatives: ";
-
-	$suggestions = "";
-	foreach ($obj2->Data as $arr) {
-
-		if ("Framed" != $arr->CombinationGroup) {
-			foreach ($arr->suggestions as $suggestion) {
-				$suggestions .= $suggestion->Combination . ", ";
-			}
-		}
+    if ("Framed" != $arr->CombinationGroup) {
+        foreach ($arr->suggestions as $suggestion) {
+            $suggestions .= $suggestion->Combination . ", ";
+        }
+    }
 
 
-	}
-	$suggestions = substr($suggestions, 0, -2);
-	print $suggestions;
-	print ".  Please respond with your prefered alternative to proceed.";
 }
+$suggestions = substr($suggestions, 0, -2);
+print $suggestions;
+print ".  Please respond with your prefered alternative to proceed.";
