45b8bf9a85f2cb73ee073e13b754060a4263aee9
Lachlan Ross <lachlan.ross@salesforce.com>
2018-11-08 18:58:31 +1100

    Added honda dealership lookup api

---
+++ honda-dealership.php
@@ -0,0 +1,40 @@
+<?php
+$combination = strtoupper($_REQUEST['combination']);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, "http://v0.postcodeapi.com.au/suburbs/$combination.json");
+$result = curl_exec($ch);
+curl_close($ch);
+
+$obj = json_decode($result);
+//var_dump($obj->Data->Available);
+
+if ($obj->Data->Available) {
+	print "Congratulations $combination is available!  Would you like to purchase this?";
+} else {
+
+	$ch2 = curl_init();
+	curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch2, CURLOPT_URL, "https://api.kiwiplates.nz/api//suggest?&startIndex=0&endIndex=8&leetSpeakAlt=true&popularNos=false&rangeOfNumbers=true&permutations=true&viewAll=false&combination=$combination");
+	$result2 = curl_exec($ch2);
+	curl_close($ch2);
+	$obj2 = json_decode($result2);
+	print "Oops! combination $combination is unavailable.  Here are some alternatives: ";
+
+	$suggestions = "";
+	foreach ($obj2->Data as $arr) {
+
+		if ("Framed" != $arr->CombinationGroup) {
+			foreach ($arr->suggestions as $suggestion) {
+				$suggestions .= $suggestion->Combination . ", ";
+			}
+		}
+
+
+	}
+	$suggestions = substr($suggestions, 0, -2);
+	print $suggestions;
+	print ".  Please respond with your prefered alternative to proceed.";
+}
